604b95d7c2446c1849e2522b3e2eb72ec8665962
Angel Magana <maganaangel342@gmail.com>
2025-04-03 13:34:30 -0600

    Graficas completadas

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ import RegisterCongressman from "./pages/Register/RegisterCongressman";
 import RegisterAuthor from "./pages/Register/RegisterAuthor";
 import Home from "./pages/Home/Home";
 import AdminstrarComite from "./pages/Administrar_Miembros_Comite/administrarComite";
-import SubirTrabajo from "./pages/Subir_Trabajo/SubirTrabajo"
+import SubirTrabajo from "./pages/Subir_Trabajo/SubirTrabajo";
 import CrearSesion from "./pages/Crear_Sesion/CrearSesion";
+import CongressForInstitution from "./components/Grafica/CongressForInstitution";
+import TopTenAutores from "./components/Grafica/TopTenAutores";
+import DistribucionTrabajosSesion from "./components/Grafica/DistribucionSesiones";
+import UsoSalasCongreso from "./components/Grafica/UsoSalasActivas";
+import RelacionesCongresistas from "./components/Grafica/RelacionesCongresistas";
+import EvolucionInscripciones from "./components/Grafica/EvolucionInscripciones";
+import EstadoTrabajos from "./components/Grafica/EstadoTrabajos";
+import NotificacionesCongreso from "./components/Grafica/NotificacionesCongreso";
+import CongresistasTrabajos from "./components/Grafica/CongresistasTrabajos";
+import MenuGraficas from "./pages/Graficas/MenuGraficas";
 
 function App() {
   return (
@@ -21,6 +31,31 @@ function App() {
         <Route path="/administrarComite" element={<AdminstrarComite />} />
         <Route path="/subirTrabajo" element={<SubirTrabajo />} />
         <Route path="/crearSesion" element={<CrearSesion />} />
+        <Route path="/menuGraficas" element={<MenuGraficas />} />
+        <Route
+          path="/congressForInstitution"
+          element={<CongressForInstitution />}
+        />
+        <Route path="/topTenAutores" element={<TopTenAutores />} />
+        <Route
+          path="/distribucionSesiones"
+          element={<DistribucionTrabajosSesion />}
+        />
+        <Route path="/usoSalasActivas" element={<UsoSalasCongreso />} />
+        <Route
+          path="/relacionesCongresistas"
+          element={<RelacionesCongresistas />}
+        />
+        <Route
+          path="/evolucionInscripciones"
+          element={<EvolucionInscripciones />}
+        />
+        <Route path="/estadoTrabajos" element={<EstadoTrabajos />} />
+        <Route path="/notificaciones" element={<NotificacionesCongreso />} />
+        <Route
+          path="/trabajosCongresistas"
+          element={<CongresistasTrabajos />}
+        />
       </Routes>
     </Router>
   );
---
+++ src/pages/Graficas/MenuGraficas.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+import "./style.css";
+import Logo from "../../assets/images/imgpng.png";
+import CongressForInstitution from "../../components/Grafica/CongressForInstitution";
+import TopTenAutores from "../../components/Grafica/TopTenAutores";
+import DistribucionTrabajosSesion from "../../components/Grafica/DistribucionSesiones";
+import UsoSalasCongreso from "../../components/Grafica/UsoSalasActivas";
+import RelacionesCongresistas from "../../components/Grafica/RelacionesCongresistas";
+import EvolucionInscripciones from "../../components/Grafica/EvolucionInscripciones";
+import EstadoTrabajos from "../../components/Grafica/EstadoTrabajos";
+import NotificacionesCongreso from "../../components/Grafica/NotificacionesCongreso";
+import CongresistasTrabajos from "../../components/Grafica/CongresistasTrabajos";
+import Footer from "../../components/Footer/Footer";
+
+const MenuGraficas = () => {
+  const [selectedGraph, setSelectedGraph] = useState(null);
+
+  const renderGraph = () => {
+    switch (selectedGraph) {
+      case "congressForInstitution":
+        return <CongressForInstitution />;
+      case "topTenAutores":
+        return <TopTenAutores />;
+      case "distribucionSesiones":
+        return <DistribucionTrabajosSesion />;
+      case "usoSalasActivas":
+        return <UsoSalasCongreso />;
+      case "relacionesCongresistas":
+        return <RelacionesCongresistas />;
+      case "evolucionInscripciones":
+        return <EvolucionInscripciones />;
+      case "estadoTrabajos":
+        return <EstadoTrabajos />;
+      case "notificaciones":
+        return <NotificacionesCongreso />;
+      case "trabajosCongresistas":
+        return <CongresistasTrabajos />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="fondo">
+      <img
+        src={Logo}
+        className="img-fluid logo"
+        alt="Logo"
+        style={{ width: "200px" }}
+      />
+      {/* Figuras decorativas */}
+      <div className="cuadrado"></div>
+      <div className="trianguloo"></div>
+      <div className="circulo"></div>
+
+      {/* Botones para navegar */}
+      <div className="buttons-container">
+        <button
+          onClick={() => setSelectedGraph("congressForInstitution")}
+          className="btn btn-primary"
+        >
+          Ver Congress For Institution
+        </button>
+        <button
+          onClick={() => setSelectedGraph("topTenAutores")}
+          className="btn btn-primary"
+        >
+          Top 10 Autores mas trabajos
+        </button>
+        <button
+          onClick={() => setSelectedGraph("distribucionSesiones")}
+          className="btn btn-primary"
+        >
+          Distribucion sesiones
+        </button>
+        <button
+          onClick={() => setSelectedGraph("usoSalasActivas")}
+          className="btn btn-primary"
+        >
+          Uso salas congreso
+        </button>
+        <button
+          onClick={() => setSelectedGraph("relacionesCongresistas")}
+          className="btn btn-primary"
+        >
+          Relacion congresistas
+        </button>
+        <button
+          onClick={() => setSelectedGraph("evolucionInscripciones")}
+          className="btn btn-primary"
+        >
+          Evolucion inscripciones
+        </button>
+        <button
+          onClick={() => setSelectedGraph("estadoTrabajos")}
+          className="btn btn-primary"
+        >
+          Estado trabajos
+        </button>
+        <button
+          onClick={() => setSelectedGraph("notificaciones")}
+          className="btn btn-primary"
+        >
+          Notificaciones
+        </button>
+        <button
+          onClick={() => setSelectedGraph("trabajosCongresistas")}
+          className="btn btn-primary"
+        >
+          Trabajos congresistas
+        </button>
+        {/* Agrega más botones aquí si es necesario */}
+      </div>
+
+      {/* Renderiza la gráfica seleccionada */}
+      <div className="graph-container w-75">{renderGraph()}</div>
+
+      <div className="sticky-bottom w-100 mb-0">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MenuGraficas;
